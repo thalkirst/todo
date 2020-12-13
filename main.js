0d4aa4580dff7cc26b96e86c965e2562b6e22e7a
thalkirst <peter.bonyai@gmail.com>
2020-12-13 09:49:25 +0100

    buttons and localstorage left to do

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// basic initialization steps //
+
+let pendingItems = 0;
+let completedItems = 0;
+const addButton = document.querySelector('.add__button');
+addButton.addEventListener('click', newListItem);
+
+// adding date and day of week to header //
+
 const addDate = () => {
     const today = new Date();
     const dayDisplayed = document.querySelector('.current__day');
@@ -10,8 +19,7 @@ const addDate = () => {
 
 addDate()
 
-let pendingItems = 0;
-let completedItems = 0;
+// function to keep track and display the number of incomplete tasks //
 
 const updatePending = () => {
     const pending = document.querySelector('.pending__items');
@@ -19,32 +27,70 @@ const updatePending = () => {
         pending.textContent = `Relax! You have nothing to do now.`
     }
     else {
-        pending.textContent = `You have ${pendingItems} pending items.`
+        pending.textContent = `You have ${pendingItems} pending item(s).`
     }
 }
 updatePending();
 
+// function to keep track and display the number of completed tasks //
 
-const addButton = document.querySelector('.add__button');
-addButton.addEventListener('click', newListItem);
+const updateCompleted = () => {
+    const completed = document.querySelector('.completed__tasks');
+    if (completedItems === 0) {
+    }
+    else {
+        completed.textContent = `You have ${completedItems} completed item(s).`
+    }
+}
+updateCompleted();
 
-function newListItem() {
-    const li = document.createElement("li");
-    const checkBox = document.createElement("input");
-    const closeButton = document.createElement("i");
-    checkBox.type = 'checkbox';
-    closeButton.classList.add('close__button', 'fa', 'fa-trash');
-    checkBox.classList.add('list__checkbox');
+// todo list item text generator //
+
+function textGenerator() {
+    const text = document.createElement('span');
     let inputValue = document.querySelector(".input__field").value;
     if (inputValue === '') {
         inputValue = document.querySelector(".input__field").placeholder;
     }
-    const text = document.createElement('span');
     text.textContent = inputValue;
     text.classList.add('list__text');
+    return text;
+}
+
+// complete list item generator //
+
+function itemGenerator(li, checkBox, text, closeButton) {
     li.appendChild(checkBox);
     li.appendChild(text);
     li.appendChild(closeButton);
+    document.querySelector(".list").appendChild(li);
+    setTimeout(function () {
+        li.classList.add("show");
+    }, 10);
+}
+
+// remove a todo list item //
+
+function removeItem(li) {
+    li.removeChild(li.lastChild);
+    document.querySelector(".list").removeChild(li);
+    pendingItems = pendingItems - 1;
+    updatePending();
+}
+
+// adding a completed item //
+
+function addCompletedItem(li) {
+    completedItems++;
+    updateCompleted();
+    document.querySelector(".list__completed").appendChild(li);
+}   
+
+
+// adding event listeners to list items //
+
+function listenerGenerator(li, checkBox, closeButton) {
+
     li.addEventListener('mouseover', () => {
         closeButton.classList.add('show');
     });
@@ -52,17 +98,35 @@ function newListItem() {
         closeButton.classList.remove('show');
     });
 
-    document.querySelector(".list").appendChild(li);
-    setTimeout(function () {
-        li.classList.add("show");
-    }, 10);
+    closeButton.addEventListener('click', () => {
+        removeItem(li);
+    });
+
+    checkBox.addEventListener('click', () => {
+        removeItem(li);
+        addCompletedItem(li);
+    });
+
+}
+
+// creating a list item from the task specified //
+
+function newListItem() {
+
+    const li = document.createElement("li");
+    const checkBox = document.createElement("input");
+    const closeButton = document.createElement("i");
+    checkBox.type = 'checkbox';
+    checkBox.classList.add('list__checkbox');
+    closeButton.classList.add('close__button', 'fa', 'fa-trash');
+    let text = textGenerator();
+
+    itemGenerator(li, checkBox, text, closeButton);
+    listenerGenerator(li, checkBox, closeButton);
+
     pendingItems++
     updatePending();
-    closeButton.addEventListener('click', () => {
-        document.querySelector(".list").removeChild(li);
-        pendingItems = pendingItems - 1;
-        updatePending();
-    });
+
     document.querySelector(".input__field").value = "";
 }
 
